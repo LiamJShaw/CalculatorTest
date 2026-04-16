09b544e4e7f7942cbc14a1573c60624e06a13f3f
Liam Shaw <liam@ljshaw.co.uk>
2026-04-16 22:32:50 +0100

    Add CORS

--- CalculatorTest.Api/Program.cs
+++ CalculatorTest.Api/Program.cs
@@ -3,6 +3,12 @@ using CalculatorTest.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(p =>
+        p.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader()
+    )
+);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
 
